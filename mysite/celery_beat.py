e521aa3d962aac44930179a875002677a59c9d15
developerkorteks <developerskorteks@gmail.com>
2025-08-03 13:23:58 +0700

    initial

--- mysite/celery_beat.py
+++ mysite/celery_beat.py
@@ -3,11 +3,11 @@ from datetime import timedelta
 
 # Jadwal tugas Celery
 CELERY_BEAT_SCHEDULE = {
-    'check-api-status-every-minute': {
-        'task': 'streamapp.tasks.check_api_status',
-        'schedule': timedelta(minutes=1),  # Setiap menit (untuk pengujian lokal)
-        'args': (),
-    },
+    # 'check-api-status-every-minute': {
+    #     'task': 'streamapp.tasks.check_api_status',
+    #     'schedule': timedelta(minutes=1),  # Setiap menit (untuk pengujian lokal)
+    #     'args': (),
+    # },
     'check-api-status-every-hour': {
         'task': 'streamapp.tasks.check_api_status',
         'schedule': crontab(minute='0', hour='*/1'),  # Setiap jam
